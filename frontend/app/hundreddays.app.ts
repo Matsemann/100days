b6d68c6f7c28d8d33409af7c212ccb600343331f
Mats Krüger Svensson <mats@matsemann.com>
2016-01-10 21:10:34 +0100

    lagde "logo", lol

--- frontend/app/hundreddays.app.ts
+++ frontend/app/hundreddays.app.ts
@@ -2,13 +2,15 @@ import {Component} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES} from "angular2/router";
 
 import {RegisterComponent} from "./register/register.component";
+import {LandingComponent} from "./landingpage/landing.component";
 
 @Component({
     selector: 'hundred-days',
-    templateUrl: 'app/hundreddaysTemplate.html',
+    template: `<router-outlet></router-outlet>`,
     directives: [ROUTER_DIRECTIVES]
 })
 @RouteConfig([
+    {path:'/', name: 'Landing', component: LandingComponent},
     {path:'/register', name: 'Register', component: RegisterComponent}
 ])
 export class HundreddaysApp {
